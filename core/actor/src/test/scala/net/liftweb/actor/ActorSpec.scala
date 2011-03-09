9d4675a25ea1b85fcbf3c891be7c0b3ba3c6f072
etorreborre <etorreborre@yahoo.com>
2011-03-10 09:13:26 +1100

    migrated the actor specs

--- core/actor/src/test/scala/net/liftweb/actor/ActorSpec.scala
+++ core/actor/src/test/scala/net/liftweb/actor/ActorSpec.scala
@@ -17,7 +17,7 @@
 package net.liftweb
 package actor
 
-import org.specs.Specification
+import org.specs2.mutable._
 
 import common._
 
@@ -25,8 +25,9 @@ import common._
 /**
  * Systems under specification for Lift Actor.
  */
-object ActorSpec extends Specification("Actor Specification") {
-
+object ActorSpec extends Specification {
+  "Actor Specification".title
+  
   "A Scala Actor" should {
     "support common features" in commonFeatures(new MyScalaActor)
   }
@@ -41,31 +42,31 @@ object ActorSpec extends Specification("Actor Specification") {
       val a = actor
       a ! Set(33)
       a !? Get()
-      (a.!?(50, Get())) must be_==(Full(Answer(33))).eventually
+      (a.!?(50, Get())) must be_===(Full(Answer(33))).eventually
     }
 
     "allow setting and getting of a value with subclass of Get()" in {
       val a = actor
       a ! Set(33)
       a ! new FunnyGet()
-      (a.!?(50L, new FunnyGet())) must be_==(Full(Answer(33))).eventually
+      (a.!?(50L, new FunnyGet())) must be_===(Full(Answer(33))).eventually
     }
 
     "allow adding of a value" in {
       val a = actor
       a ! Set(33)
-      (a !< Add(44)).get(50) must be_==(Full(Answer(77))).eventually
+      (a !< Add(44)).get(50) must be_===(Full(Answer(77))).eventually
     }
 
     "allow subtracting of a value" in {
       val a = actor
       a ! Set(33)
-      (a !< Sub(11)).get(50) must be_==(Full(Answer(22))).eventually
+      (a !< Sub(11)).get(50) must be_===(Full(Answer(22))).eventually
     }
 
     "properly timeout" in {
       val a = actor
-      (a !< Set(33)).get(50) must be_==(Empty).eventually
+      (a !< Set(33)).get(50) must be_===(Empty).eventually
     }
   }
 
